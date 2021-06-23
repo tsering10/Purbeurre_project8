ad68998efa9b6299ccc9b1459aae3a53c13462b5
tashi tsering <tashi.cse@gmail.com>
2021-06-23 21:48:44 +0200

    unwanted import and email display removed

--- purbeurre/forms.py
+++ purbeurre/forms.py
@@ -1,5 +1,4 @@
 # Imports
-from django import forms
 from django.forms import ModelForm, TextInput
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
